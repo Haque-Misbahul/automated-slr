42c092b020eb0cf42533ce1b4eb2fb8a843ba28a
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-11-09 18:29:04 +0100

    Select sources  page disign updated

--- slr/ui/pages/03_select_sources.py
+++ slr/ui/pages/03_select_sources.py
@@ -3,11 +3,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 
 import json
 import streamlit as st
+from slr.ui.theme import inject_css
 
 # ---------------- Page setup ----------------
 st.set_page_config(page_title="Planning → Step 3: Select Sources", layout="wide")
+inject_css()
 st.markdown(
-    "<h2 style='margin-top:25px;'>🗂️ Planning • Step 3: Select Digital Library Sources</h2>",
+    "<h2 style='margin-top:25px;'>Planning • Step 3: Select Digital Library Sources</h2>",
     unsafe_allow_html=True,
 )
 
